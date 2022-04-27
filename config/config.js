a48979bba563629c18a484d8031d39194fe21482
ilwel <ilwelisaac@hotmail.com>
2022-04-27 02:28:24 -0300

    migrations config

---
+++ config/config.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const { DB_DATABASE, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_DIALECT, DB_PORT } = process.env;
+module.exports =
+{
+  "development": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_DATABASE,
+    "host": DB_HOST,
+    "port": DB_PORT,
+    "dialect": DB_DIALECT
+  },
+  "test": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_DATABASE,
+    "host": DB_HOST,
+    "port": DB_PORT,
+    "dialect": DB_DIALECT
+  },
+  "production": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_DATABASE,
+    "host": DB_HOST,
+    "port": DB_PORT,
+    "dialect": DB_DIALECT,
+    "logging": false
+  }
+}
